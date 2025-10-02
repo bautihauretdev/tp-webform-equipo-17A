e5831836acdfcb04e9cbcbe7cb141cca7bb4d690
natudellasavia <80315756+natudellasavia@users.noreply.github.com>
2025-10-01 21:07:08 -0300

    implementacion de arquitectura en capas 'dominio' y 'negocio' con cls Marca y MarcaNegocio

---
+++ tp-webform-equipo-17A/dominio/Marca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        // ATRIBUTOS
+        public int id { get; set; } // Autoincrementable tomando de referencia el último que encuentra
+        public string descripcion { get; set; }
+
+        // CONSTRUCTOR SIN PARÁMETROS
+        public Marca() { }
+
+        // CONSTRUCTOR CON PARÁMETROS
+        public Marca(int id, string descripcion)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+        }
+    }
+}
---
+++ tp-webform-equipo-17A/negocio/MarcaNegocio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class MarcaNegocio
+    {
+        // Descomentar cuando se desarrolle la clase AccesoDatos
+
+        //public List<Marca> Listar()
+        //{
+        //    List<Marca> lista = new List<Marca>();
+        //    AccesoDatos datos = new AccesoDatos();
+
+        //    try
+        //    {
+        //        string consulta = "SELECT Id, Descripcion FROM MARCAS";
+        //        datos.setearConsulta(consulta);
+        //        SqlDataReader lector = datos.ejecutarLectura();
+
+
+        //        while (lector.Read())
+        //        {
+        //            int id = (int)lector["Id"];
+        //            string descripcion = lector["Descripcion"].ToString();
+        //            lista.Add(new Marca(id, descripcion));
+        //        }
+        //        datos.cerrarConexion();
+        //        return lista;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
