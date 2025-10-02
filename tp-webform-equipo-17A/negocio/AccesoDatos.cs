17b7b5619dee136cb7696bb6b34106f72cd560ff
Axel29071 <Batistuta622@gmail.com>
2025-10-02 10:07:51 -0300

    Implementacion CategoriaNegocio y AcessoDatos

--- tp-webform-equipo-17A/negocio/AccesoDatos.cs
+++ tp-webform-equipo-17A/negocio/AccesoDatos.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using System.Configuration;
 
-namespace negocio
+
+namespace TPWebForm_equipo_17A.Data
 {
     public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+
+        public SqlDataReader Lector
+        {
+
+            get { return lector; }
+
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=localhost\\SQLEXPRESS; database=PROMOS_DB; integrated security= true");
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.Parameters.Clear();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public SqlDataReader ejecutarLectura()
+        {
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+                return lector;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor ?? DBNull.Value);
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
     }
 }
---
+++ tp-webform-equipo-17A/negocio/CategoriaNegocio.cs
@@ -0,0 +1,42 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    internal class CategoriaNegocio
+    {
+        public List<Categoria> Listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT Id, Descripcion FROM CATEGORIAS");
+                SqlDataReader lector = datos.ejecutarLectura();
+
+                while (lector.Read())
+                {
+                    Categoria cat = new Categoria();
+                    cat.Id = (int)lector["Id"];
+                    cat.Descripcion = lector["Descripcion"].ToString();
+                    lista.Add(cat);
+                }
+
+                return lista;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
