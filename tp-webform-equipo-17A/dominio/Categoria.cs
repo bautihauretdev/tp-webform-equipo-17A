ec054501322446fd880cc311325fe31c00c8369f
Axel29071 <Batistuta622@gmail.com>
2025-10-02 09:48:58 -0300

    Agregacion de cls Categoria y Voucher

---
+++ tp-webform-equipo-17A/dominio/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Categoria
+    {
+        // ATRIBUTOS
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        // CONSTRUCTOR
+        public Categoria() { }
+
+        public Categoria(int id, string descripcion)
+        {
+            this.Id = id;
+            this.Descripcion = descripcion;
+        }
+    }
+}
