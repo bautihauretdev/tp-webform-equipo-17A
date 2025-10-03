4caaf887498de5139e8df5fc3831506a4fc7abe6
natudellasavia <80315756+natudellasavia@users.noreply.github.com>
2025-10-03 14:16:17 -0300

    mejoras visuales MasterPage y Premios

--- tp-webform-equipo-17A/presentacionWebForm/script PROMO_DB.sql
+++ tp-webform-equipo-17A/presentacionWebForm/script PROMO_DB.sql
@@ -150,6 +150,7 @@ SELECT * FROM Vouchers
 
 SELECT * FROM ARTICULOS
 
+SELECT * FROM IMAGENES
 
 SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, A.IdMarca, M.Descripcion as Marca, A.IdCategoria, C.Descripcion as Categoria, A.Precio, 
 (SELECT TOP 1 ImagenUrl FROM IMAGENES I WHERE I.IdArticulo = A.Id) as ImagenUrl
